4b29ab65435d82cbd30b77bea3b5f3bf42ab3570
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2026-02-21 23:17:00 -0600

    feat(web): top menu and slidebar [SF-45] (#16)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
 import ToastContainer from "./components/organisms/ToastContainer.vue";
+import MenuTop from "./components/molecules/MenuTop.vue";
+import SidebarMenu from "./components/molecules/SidebarMenu.vue";
+
+import { useAuthStore } from "./stores/authStore";
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const authStore = useAuthStore();
+const route = useRoute();
+
+const isAuthenticated = computed(() => {
+    return (
+        authStore.isAuthenticated &&
+        !["login", "register"].includes(String(route.name))
+    );
+});
 </script>
 
 <template>
-    <router-view />
-    <ToastContainer />
+    <template v-if="isAuthenticated">
+        <MenuTop />
+        <SidebarMenu />
+    </template>
+    <div :class="{ 'main-content': isAuthenticated }">
+        <router-view />
+        <ToastContainer />
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main-content {
+    padding-top: 4.5rem;
+    padding-left: 4rem;
+}
+</style>
