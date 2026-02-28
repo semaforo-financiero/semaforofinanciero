1c92768065baaf723af6ac628874b0da0c2fc965
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2026-02-27 23:52:24 -0600

    feat(web): gateway to get and update profile info [SF-27] (#21)

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,10 +1,13 @@
 import UserAPI from "./user/UserAPI";
+import UserProfileAPI from "./userProfile/UserProfileAPI";
 
 class API {
     user: UserAPI;
+    profile: UserProfileAPI;
 
     constructor() {
         this.user = new UserAPI();
+        this.profile = new UserProfileAPI();
     }
 }
 
