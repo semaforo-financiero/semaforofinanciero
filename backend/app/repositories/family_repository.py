02a43b952daa834c91e0f3d352e73fd9058d696a
SergioClaustro <sergio.claustro5250@alumnos.udg.mx>
2026-02-28 23:51:57 -0600

    Sf 38 (#26)

--- backend/app/repositories/family_repository.py
+++ backend/app/repositories/family_repository.py
@@ -17,6 +17,19 @@ class FamilyRepository:
             logger.exception("Error inserting family")
             raise
     
+    def update_family(self, name: str, family_id: str):
+        try:
+            return (
+                self.supabase
+                .table("families")
+                .update({"name": name})
+                .eq("id", family_id)
+                .execute()
+            )
+        except Exception:
+            logger.exception("Error updating family")
+            raise
+    
     def delete_family(self, family_id: str):
         try:
             return (
@@ -135,4 +148,3 @@ class FamilyRepository:
         except Exception:
             logger.exception("Error getting user invitations")
             raise
-
