7f8b636cc4a3690beb84d4cd0afb55ae7c52b439
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2026-03-04 22:27:26 -0600

    feat(web): family view [SF-29] (#30)

---
+++ frontend/src/assets/icons/PaperFly.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        width="16"
+        height="16"
+    >
+        <line x1="22" y1="2" x2="11" y2="13" />
+        <polygon points="22 2 15 22 11 13 2 9 22 2" />
+    </svg>
+</template>
