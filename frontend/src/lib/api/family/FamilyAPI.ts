210712b9733f9b610f6b9fdd1baa74ccee6e297d
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2026-03-04 22:27:26 -0600

    feat(web): family view [SF-29] (#30)

--- frontend/src/lib/api/family/FamilyAPI.ts
+++ frontend/src/lib/api/family/FamilyAPI.ts
@@ -26,6 +26,69 @@ class FamilyAPI {
 
         return family.id;
     }
+
+    async getFamily(token: string): Promise<Family> {
+        const url = import.meta.env.VITE_API_BASE_URL + "api/families";
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            const error = await response.json().catch(() => ({}));
+            throw new Error(
+                error.message ||
+                    "Error al obtener la información de la familia",
+            );
+        }
+
+        const familyData = await response.json();
+
+        return new Family(familyData);
+    }
+
+    async inviteMember(email: string, token: string): Promise<void> {
+        const url = import.meta.env.VITE_API_BASE_URL + "api/families/invite";
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                email: email,
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error(response.status.toString());
+        }
+    }
+
+    async removeMember(userId: string, token: string): Promise<void> {
+        const url =
+            import.meta.env.VITE_API_BASE_URL +
+            `api/families/members/${userId}`;
+
+        const response = await fetch(url, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            const error = await response.json().catch(() => ({}));
+            throw new Error(
+                error.message || "Error al eliminar al miembro de la familia",
+            );
+        }
+    }
 }
 
 export default FamilyAPI;
