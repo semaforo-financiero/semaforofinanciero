5641bb82ac92e9c5de3aee073ef3cb89f77294cd
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2026-03-23 18:51:06 -0600

    feat(web): income amounts view [SF-65] (#42)

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -8,25 +8,25 @@ import UserAPI from "./user/UserAPI";
 import UserProfileAPI from "./userProfile/UserProfileAPI";
 
 class API {
-    user: UserAPI;
-    profile: UserProfileAPI;
-    family: FamilyAPI;
-    invitation: InvitationAPI;
-    income: IncomeAPI;
-    expense: ExpenseAPI;
-    incomeAmount: IncomeAmountAPI;
-    expenseAmount: ExpenseAmountAPI;
+  user: UserAPI;
+  profile: UserProfileAPI;
+  family: FamilyAPI;
+  invitation: InvitationAPI;
+  income: IncomeAPI;
+  expense: ExpenseAPI;
+  incomeAmount: IncomeAmountAPI;
+  expenseAmount: ExpenseAmountAPI;
 
-    constructor() {
-        this.user = new UserAPI();
-        this.profile = new UserProfileAPI();
-        this.family = new FamilyAPI();
-        this.invitation = new InvitationAPI();
-        this.income = new IncomeAPI();
-        this.expense = new ExpenseAPI();
-        this.incomeAmount = new IncomeAmountAPI();
-        this.expenseAmount = new ExpenseAmountAPI();
-    }
+  constructor() {
+    this.user = new UserAPI();
+    this.profile = new UserProfileAPI();
+    this.family = new FamilyAPI();
+    this.invitation = new InvitationAPI();
+    this.income = new IncomeAPI();
+    this.expense = new ExpenseAPI();
+    this.incomeAmount = new IncomeAmountAPI();
+    this.expenseAmount = new ExpenseAmountAPI();
+  }
 }
 
 const api = new API();
