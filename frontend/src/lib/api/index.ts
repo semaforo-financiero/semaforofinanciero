7cd2430f8c85dd7d9c39bd9d1763dea332795c60
Manuel Alejandro Rodriguez Sanchez <manuel.rs003@gmail.com>
2026-03-26 01:31:44 -0600

    feat(web): home view

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -4,29 +4,32 @@ import FamilyAPI from "./family/FamilyAPI";
 import IncomeAPI from "./income/IncomeAPI";
 import IncomeAmountAPI from "./incomeAmount/IncomeAmountAPI";
 import InvitationAPI from "./invitations/InvitationAPI";
+import RiskAPI from "./risk/RiskAPI";
 import UserAPI from "./user/UserAPI";
 import UserProfileAPI from "./userProfile/UserProfileAPI";
 
 class API {
-  user: UserAPI;
-  profile: UserProfileAPI;
-  family: FamilyAPI;
-  invitation: InvitationAPI;
-  income: IncomeAPI;
-  expense: ExpenseAPI;
-  incomeAmount: IncomeAmountAPI;
-  expenseAmount: ExpenseAmountAPI;
+    user: UserAPI;
+    profile: UserProfileAPI;
+    family: FamilyAPI;
+    invitation: InvitationAPI;
+    income: IncomeAPI;
+    expense: ExpenseAPI;
+    incomeAmount: IncomeAmountAPI;
+    expenseAmount: ExpenseAmountAPI;
+    risk: RiskAPI;
 
-  constructor() {
-    this.user = new UserAPI();
-    this.profile = new UserProfileAPI();
-    this.family = new FamilyAPI();
-    this.invitation = new InvitationAPI();
-    this.income = new IncomeAPI();
-    this.expense = new ExpenseAPI();
-    this.incomeAmount = new IncomeAmountAPI();
-    this.expenseAmount = new ExpenseAmountAPI();
-  }
+    constructor() {
+        this.user = new UserAPI();
+        this.profile = new UserProfileAPI();
+        this.family = new FamilyAPI();
+        this.invitation = new InvitationAPI();
+        this.income = new IncomeAPI();
+        this.expense = new ExpenseAPI();
+        this.incomeAmount = new IncomeAmountAPI();
+        this.expenseAmount = new ExpenseAmountAPI();
+        this.risk = new RiskAPI();
+    }
 }
 
 const api = new API();
